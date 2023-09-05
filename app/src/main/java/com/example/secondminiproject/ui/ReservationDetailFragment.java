e3217cadbf36f5a31e2dbe265b44f2c193e8c89e
pjhong1004 <pgc1004@gmail.com>
2023-09-05 18:30:43 +0900

    0905 재홍파트 작성

--- app/src/main/java/com/example/secondminiproject/ui/ReservationDetailFragment.java
+++ app/src/main/java/com/example/secondminiproject/ui/ReservationDetailFragment.java
@@ -3,64 +3,37 @@ package com.example.secondminiproject.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.secondminiproject.R;
+import com.example.secondminiproject.databinding.FragmentReservationDetailBinding;
+import com.example.secondminiproject.databinding.FragmentReservationListBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReservationDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ReservationDetailFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "ReservationDetailFragment";
+    private FragmentReservationDetailBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ReservationDetailFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReservationDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReservationDetailFragment newInstance(String param1, String param2) {
-        ReservationDetailFragment fragment = new ReservationDetailFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private NavController navController;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentReservationDetailBinding.inflate(inflater);
+
+        navController = NavHostFragment.findNavController(this);
+
+        initBtnReservationDetailFragmentMyPage();
+        return binding.getRoot();
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reservation_detail, container, false);
+    private void initBtnReservationDetailFragmentMyPage() {
+        binding.btnReservationDetailReservationList.setOnClickListener(v -> {
+            navController.popBackStack();
+        });
     }
 }
