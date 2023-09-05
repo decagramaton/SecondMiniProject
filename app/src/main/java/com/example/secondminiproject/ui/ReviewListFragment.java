0dfd72e207d32ef679153a7a59ea8407513026b8
JJNSC <whdwls963@gmail.com>
2023-09-05 18:27:02 +0900

    230905 1826 jjk 교수님 이동 과제 종진파트 작성

--- app/src/main/java/com/example/secondminiproject/ui/ReviewListFragment.java
+++ app/src/main/java/com/example/secondminiproject/ui/ReviewListFragment.java
@@ -3,64 +3,52 @@ package com.example.secondminiproject.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.secondminiproject.R;
+import com.example.secondminiproject.databinding.FragmentReviewListBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReviewListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ReviewListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ReviewListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReviewListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReviewListFragment newInstance(String param1, String param2) {
-        ReviewListFragment fragment = new ReviewListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final String TAG = "ReviewListFragment";
+    private FragmentReviewListBinding binding;
+    private NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        navController = NavHostFragment.findNavController(this);
+
+        binding = FragmentReviewListBinding.inflate(inflater);
+
+        initBtnMyPage();
+        initBtnReviewWrite();
+        initBtnReviewUpdate();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_review_list, container, false);
+        return binding.getRoot();
+    }
+
+    private void initBtnMyPage() {
+        binding.btnReviewListMyPage.setOnClickListener(v -> {
+            navController.popBackStack();
+        });
+    }
+
+    private void initBtnReviewWrite() {
+        binding.btnReviewListReviewWrite.setOnClickListener(v -> {
+            navController.navigate(R.id.action_dest_review_list_to_dest_review_write);
+        });
+    }
+
+    private void initBtnReviewUpdate() {
+        binding.btnReviewListReviewUpdate.setOnClickListener(v -> {
+            navController.navigate(R.id.action_dest_review_list_to_dest_review_update);
+        });
     }
 }
